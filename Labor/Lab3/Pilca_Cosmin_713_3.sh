5f4151ad9cbd9008ccc706187082cca61127423d
CosminPilca <cosminpilca@gmail.com>
2024-03-18 04:57:45 +0200

    Added by Cosmin

--- Labor/Lab3/Pilca_Cosmin_713_3.sh
+++ Labor/Lab3/Pilca_Cosmin_713_3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <folder_path>"
+    echo "Verwendung: $0 <Ordnerpfad>"
     exit 1
 fi
+
 
 folder_path="$1"
 
@@ -16,15 +18,15 @@ sorted_by_access_time=$(ls -lt "$folder_path" | awk '{print $9}')
 
 sorted_by_byte_size=$(ls -lS "$folder_path" | awk '{print $9}')
 
-
-echo "Sorted by filename:"
+# Anzeigen erstellen
+echo "Sortiert nach Dateiname:"
 echo "$sorted_by_filename"
 echo
 
-echo "Sorted by access time:"
+echo "Sortiert nach Zugriffszeit:"
 echo "$sorted_by_access_time"
 echo
 
-echo "Sorted by byte size:"
+echo "Sortiert nach Bytegröße:"
 echo "$sorted_by_byte_size"
 echo
